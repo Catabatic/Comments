8b942e85c55abab861207694d467e13cde2ba4ba
Catabatic <linus@tollgard.com>
2021-08-04 03:32:04 +0200

    - Youtube comment replies are now mimiced in discord as discord replies - Save replies and check duplicates to prevent risk of sending the same message to discord channel multiple times, also for testing purposes if checking for duplicates even works in the first place - Fixed DISCORD_CHANNEL env variable was a string - Fixed YOUTUBE_CHANNEL_ID exist validation, would not call commentThreads for videoId even if YOUTUBE_CHANNEL_ID is not defined

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from googleapiclient.discovery import build
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
-CHANNEL = os.getenv("DISCORD_CHANNEL")
+CHANNEL = int(os.getenv("DISCORD_CHANNEL"))
 YOUTUBE_TOKEN = os.getenv("YOUTUBE_API_KEY")
 YOUTUBE_CHANNEL_ID = os.getenv("YOUTUBE_CHANNEL_ID")
 YOUTUBE_VIDEO_ID = os.getenv("YOUTUBE_VIDEO_ID")
@@ -19,10 +19,16 @@ YOUTUBE_VIDEO_ID = os.getenv("YOUTUBE_VIDEO_ID")
 youtube = build("youtube", "V3", developerKey=YOUTUBE_TOKEN)
 
 request = {}
-request = youtube.commentThreads().list(
-    part="snippet",
-    videoId=YOUTUBE_VIDEO_ID
-)
+if not YOUTUBE_CHANNEL_ID:
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId=YOUTUBE_VIDEO_ID
+    )
+else:
+    request = youtube.commentThreads().list(
+        part="snippet",
+        allThreadsRelatedToChannelId=YOUTUBE_CHANNEL_ID
+    )
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -30,12 +36,12 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    channel = client.get_channel(284033672292597761)
+    channel = client.get_channel(CHANNEL)
 
     scanForComments = True
     oldCommentThreads = []
 
-    async def get_and_send_replies(commentId, videoTitle, commentThreadAuthorDisplayName):
+    async def get_and_send_replies(commentId, videoTitle, commentThreadAuthorDisplayName, replyToMessage):
         repliesRequest = youtube.commentThreads().list(
             part="replies",
             id=commentId
@@ -44,11 +50,14 @@ async def on_ready():
 
         repliesMetaData = repliesResponse["items"][0]["replies"]["comments"]
         for replyMetaData in reversed(repliesMetaData):
-            replyData = replyMetaData["snippet"]
-            embedVar = discord.Embed(title=f"{videoTitle}", description=f"Replied to **{commentThreadAuthorDisplayName}**\nhttps://www.youtube.com/watch?v={replyData['videoId']}&lc={replyMetaData['id']}", color=0x00ff00)
-            embedVar.set_image(url=replyData['authorProfileImageUrl'])
-            embedVar.add_field(name=f"{replyData['authorDisplayName']}", value=f"{replyData['textOriginal']}", inline=False)
-            await channel.send(embed=embedVar)
+            if replyMetaData not in oldCommentThreads:
+                replyData = replyMetaData["snippet"]
+                embedVar = discord.Embed(title=f"{videoTitle}", description=f"Replied to **{commentThreadAuthorDisplayName}**\nhttps://www.youtube.com/watch?v={replyData['videoId']}&lc={replyMetaData['id']}", color=0x00ff00)
+                embedVar.set_image(url=replyData['authorProfileImageUrl'])
+                embedVar.add_field(name=f"{replyData['authorDisplayName']}", value=f"{replyData['textOriginal']}", inline=False)
+                await channel.send(embed=embedVar, reference=replyToMessage)
+
+                oldCommentThreads.append(repliesMetaData)
 
     while scanForComments:
         response = request.execute()
@@ -69,10 +78,10 @@ async def on_ready():
                 embedVar = discord.Embed(title=f"{videoTitle}", description=f"Comment\nhttps://www.youtube.com/watch?v={commentData['videoId']}&lc={commentMetaData['id']}", color=0x00ff00)
                 embedVar.set_image(url=commentData['authorProfileImageUrl'])
                 embedVar.add_field(name=f"{commentData['authorDisplayName']}", value=f"{commentData['textOriginal']}", inline=False)
-                await channel.send(embed=embedVar)
+                message = await channel.send(embed=embedVar)
 
                 if commentThreadData["totalReplyCount"] > 0:
-                    await get_and_send_replies(commentMetaData["id"], videoTitle, commentData['authorDisplayName'])
+                    await get_and_send_replies(commentMetaData["id"], videoTitle, commentData['authorDisplayName'], message.to_reference())
                 
                 oldCommentThreads.append(commentThread)
 
